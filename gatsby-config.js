7722889d76038d8a1a90f93c544ef0b90080b2bb
Bryan Hickey <bryanjhickey@users.noreply.github.com>
2022-02-17 11:47:50 +1100

    fix: deployiong ga analytcs

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,5 +69,37 @@ module.exports = {
         generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          // Google Analytics / GA
+          // process.env.GA_TRACKING_CODE,
+          // Google Ads / Adwords / AW
+          // "AW-CONVERSION_ID",
+          // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+          // "DC-FLOODIGHT_ID",
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          // optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          // origin: "YOUR_SELF_HOSTED_ORIGIN",
+        },
+      },
+    },
   ],
 };
