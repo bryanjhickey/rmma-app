93a9ebe1bd1b2e113951f1ddc4174b6b2952bb62
Bryan Hickey <bryanjhickey@users.noreply.github.com>
2022-02-21 14:47:53 +1100

    feat: added tagmanager plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,35 +70,10 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: 'gatsby-plugin-google-tagmanager',
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          // Google Analytics / GA
-          process.env.GA_TRACKING_CODE,
-          // Google Ads / Adwords / AW
-          // "AW-CONVERSION_ID",
-          // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-          // "DC-FLOODIGHT_ID",
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: process.env.GA_OPT_CONTAINER_ID,
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          // exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          // origin: "YOUR_SELF_HOSTED_ORIGIN",
-        },
+        id: process.env.GOOGLE_TAGMANAGER_ID,
+        includeInDevelopment: false,
       },
     },
   ],
